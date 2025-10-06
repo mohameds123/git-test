2aaa14c38d5afecb1612eae40379e53fee67beef
youssefadly <youssefadly332@gmail.com>
2025-10-06 20:53:53 +0300

    add forget paswword screen

---
+++ lib/screens/forget_password_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordScreen extends StatelessWidget {
+  const ForgetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Forget Password Screen", style: TextStyle(fontSize: 32),),
+      ),
+    );
+  }
+}
